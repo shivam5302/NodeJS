7dcd8122f9dfaa3ad251be142003fd20aae4b157
shivam5302 <shivamsoniss9549@gmail.com>
2021-03-26 09:27:16 +0530

    login and signup-> dashboard

---
+++ loginSignupDashboard/src/configs/viewEngine.js
@@ -0,0 +1,12 @@
+const express = require('express');
+// import express from "express";
+/**
+ * Config view engine for app
+ */
+let configViewEngine = (app)=> {
+    app.use(express.static("./src/public"));
+    app.set("view engine", "ejs");
+    app.set("views","./src/views");
+};
+
+module.exports = configViewEngine;
